086646d493ef53d6bfe45fcb48ebb7dfc98ec81d
Fedor Korotkov <fedor.korotkov@gmail.com>
2019-04-25 11:32:12 -0400

    Check exposed ports

--- kubernetes/dsl/src/test/kotlin/com/fkorotkov/kubernetes/SimpleCompilationTest.kt
+++ kubernetes/dsl/src/test/kotlin/com/fkorotkov/kubernetes/SimpleCompilationTest.kt
@@ -38,6 +38,7 @@ class SimpleCompilationTest {
     }
     assertEquals(serviceName, myService.metadata.name)
     assertEquals("NodePort", myService.spec.type)
+    assertEquals(1, myService.spec.ports.size)
 
     myService.apply {
       metadata {
