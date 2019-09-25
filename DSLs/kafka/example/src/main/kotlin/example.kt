b728449e3769aa5aa18b87b71ed3d8145c315707
fedor <fedor.korotkov@gmai.com>
2019-09-25 10:08:56 -0400

    Generation fixes

--- DSLs/kafka/example/src/main/kotlin/example.kt
+++ DSLs/kafka/example/src/main/kotlin/example.kt
@@ -1,9 +1,7 @@
-import com.fkorotkov.kubernetes.kafka.kafka
 import io.fabric8.kubernetes.client.DefaultKubernetesClient
 
 
 fun main() {
   val client = DefaultKubernetesClient().inNamespace("default")
   client.services()
-  client.kafka().kafkaclusters().list()
 }
