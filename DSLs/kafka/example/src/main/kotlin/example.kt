2f63b78a0811efebc15ad21bec425f0fb06ace4f
fedor <fedor.korotkov@gmai.com>
2019-09-28 14:41:06 -0400

    Generated DSL

--- DSLs/kafka/example/src/main/kotlin/example.kt
+++ DSLs/kafka/example/src/main/kotlin/example.kt
@@ -1,7 +1,21 @@
 import com.fkorotkov.kubernetes.kafka.client.DefaultKafkaClient
+import com.fkorotkov.kubernetes.kafka.metadata
+import com.fkorotkov.kubernetes.kafka.newKafkaCluster
+import com.fkorotkov.kubernetes.kafka.spec
 
 
 fun main() {
-  val client = DefaultKafkaClient().inNamespace("default")
-  client.kafkaClusters().list()
+  val client = DefaultKafkaClient().inNamespace("operator")
+  println(client.kafkaClusters().list())
+  client.kafkaClusters().create(
+      newKafkaCluster {
+        metadata {
+          name = "kafka"
+        }
+        spec {
+          replicas = 3
+        }
+      }
+  )
+  println(client.kafkaClusters().list())
 }
