222bd5db82fbcb6f0e5963e3083b32c491026fc1
fedor <fedor.korotkov@gmai.com>
2019-09-28 16:17:37 -0400

    Better example

--- DSLs/kafka/example/src/main/kotlin/example.kt
+++ DSLs/kafka/example/src/main/kotlin/example.kt
@@ -6,5 +6,16 @@ import com.fkorotkov.kubernetes.kafka.spec
 
 fun main() {
   val client = DefaultKafkaClient().inNamespace("operator")
-  client.kafkaClusters().list().items.forEach { println(it) }
+  println(client.kafkaClusters().list())
+  client.kafkaClusters().create(
+      newKafkaCluster {
+        metadata {
+          name = "kafka"
+        }
+        spec {
+          replicas = 3
+        }
+      }
+  )
+  println(client.kafkaClusters().list())
 }
